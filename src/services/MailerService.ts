d38cc6ae00dc89e78fef4442f69d8af95b64cb5d
Pedro Paes <iacapuca@gmail.com>
2020-02-06 16:49:23 -0300

    Updated MailerService

--- src/services/MailerService.ts
+++ src/services/MailerService.ts
@@ -1,61 +1,48 @@
 import cron from "node-cron"
 import signale from "signale"
-import * as nodemailer from "nodemailer"
+import sgMail from "@sendgrid/mail"
 import PDFService from "./PDFService"
 
-// TODO: Implement mailer and send reportMail every week
-// Send reports for the current week, if there are not reports, then send a message sayind that
+// TODO: Create Email facade and use OOP better practices on this Service
 
 class MailerService {
-    private config;
-
-    private _transporter: nodemailer.Transporter;
-
-    private mailOptions;
+    protected sendGrid: any;
 
     private _PDFService: PDFService;
 
+    /**
+     * @constructor
+     */
     public constructor () {
       this._PDFService = new PDFService()
-      this._transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        auth: {
-          user: "florencio.fritsch@ethereal.email",
-          pass: "e3Pexw282zmsDWSm9C"
-        }
-      })
+      this.sendGrid = sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-      this.mailOptions = {
-        from: "from_test@gmail.com",
-        to: "iacapuca@gmail.com",
-        subject: "Hello",
-        text: "Hello from node.js"
-      }
-
-      cron.schedule("* * * * *", () => {
+      cron.schedule("*/10 * * * * *", () => {
         signale.info("Sending email report")
-        this.sendMail("iacapuca@gmail.com", "Email test", "Testando envio do email")
+        this.sendMail("iacapuca@gmail.com", "no-reply@bicibot.org", "Email test", "Testando envio do email")
       })
     }
 
-    public async sendMail (to: string, subject: string, content: string): Promise<void> {
-      this.mailOptions.to = to
-      this.mailOptions.subject = subject
-      this.mailOptions.text = content
-      this.mailOptions.attachments = [{ filename: "report.pdf", content: await this._PDFService.generateReport() }]
+    public async sendMail (to: string, from: string, subject: string, content: string): Promise<void> {
+      const pdf = await this._PDFService.generateReport()
+      const msg = { to: to,
+        from: from,
+        subject: subject,
+        text: content,
+        attachments: [
+          {
+            content: Buffer.from(pdf).toString("base64"),
+            filename: "report.pdf",
+            type: "application/pdf",
+            disposition: "attachment"
+          }
+        ]
+      }
 
-      return new Promise<void>((resolve: (msg: any) => void,
-        reject: (err: Error) => void) => {
-        this._transporter.sendMail(
-          this.mailOptions, (error, info) => {
-            if (error) {
-              signale.fatal(`error: ${error}`)
-              reject(error)
-            } else {
-              signale.success(`Message Sent ${info.response}`)
-            }
-          })
+      sgMail.send(msg).then(() => {
+        signale.success("Email sent")
+      }, err => {
+        signale.fatal(err)
       })
     }
 }
